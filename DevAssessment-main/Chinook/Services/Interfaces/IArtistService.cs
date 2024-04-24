4491ffcb7bd94fc59306cd1c4c6266ec28a30647
Shazna Mohamed <ShaznaM@compello.com>
2024-04-24 11:41:03 +0530

    ArtistService added and moved artist related data retrieval methods to the service

---
+++ DevAssessment-main/Chinook/Services/Interfaces/IArtistService.cs
@@ -0,0 +1,13 @@
+﻿using Chinook.ClientModels;
+using Chinook.Models;
+
+namespace Chinook.Services.Interfaces
+{
+    public interface IArtistService
+    {
+        public Task<List<Artist>> GetArtists();
+        public Task<List<Album>> GetAlbumsForArtist(int artistId);
+        public Task<List<PlaylistTrack>> GetTracksForArtist(long artistId, string currentUserId);
+        public Task<Artist> GetSelectedArtist(long artistId);
+    }
+}
