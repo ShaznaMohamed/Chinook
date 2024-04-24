e20602adf85e5dd706e9e212b75c71cdc0231146
Shazna Mohamed <ShaznaM@compello.com>
2024-04-24 19:28:58 +0530

    Added My favorite tracks playlist and add to favorite feature implemented

--- DevAssessment-main/Chinook/Services/Interfaces/IPlaylistService.cs
+++ DevAssessment-main/Chinook/Services/Interfaces/IPlaylistService.cs
@@ -3,5 +3,7 @@
     public interface IPlaylistService
     {
         public Task<ClientModels.Playlist> GetPlaylist(long playlistId, string currentUserId);
+        public Task<bool> AddTrackToFavoritePlaylist(long trackId, string currentUserId)
+;
     }
 }
--- DevAssessment-main/Chinook/Services/PlaylistService.cs
+++ DevAssessment-main/Chinook/Services/PlaylistService.cs
@@ -1,4 +1,5 @@
-﻿using Chinook.Services.Interfaces;
+﻿using Chinook.Models;
+using Chinook.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Chinook.Services
@@ -24,6 +25,55 @@ namespace Chinook.Services
                 }).ToList()
             })
             .FirstOrDefault();
+        }
+
+        public async Task<bool> AddTrackToFavoritePlaylist(long trackId, string currentUserId)
+        {
+            try
+            {
+                var dbContext = await _dbFactory.CreateDbContextAsync();
+                var currentUser = dbContext.Users.FirstOrDefault(u => u.Id == currentUserId);
+
+                long favoritPlaylistId = 100000;  // Added random long value for unique id
+
+                var myFavoritePlaylist = new Chinook.Models.Playlist()      // Create a new playlist named My favorite tracks
+                {
+                    PlaylistId = favoritPlaylistId,
+                    Name = "My favorite tracks"
+                };
+                var favoritePlaylist = dbContext.Playlists.Any(p => p.Name == "My favorite tracks" && p.PlaylistId == favoritPlaylistId);      // Check whether a playlist named 'My favorite tracks' already exists
+                if (!favoritePlaylist)
+                {
+                    dbContext.Playlists.Add(myFavoritePlaylist);   // Add my favorite track playlist to the playlists
+                }
+
+
+                var selectedTrack = dbContext.Tracks.Include(p => p.Playlists).FirstOrDefault(t => t.TrackId == trackId);                   // Get the selected track
+                if (selectedTrack != null && !selectedTrack.Playlists.Any(p => p.PlaylistId == favoritPlaylistId && p.Name == "My favorite tracks"))    // Add the selected track in to my favorite playlist if it is not found already
+                {
+                    selectedTrack.Playlists.Add(myFavoritePlaylist);
+                    dbContext.Playlists.Attach(myFavoritePlaylist);
+                }
+
+                var currentUserPlayList = dbContext.UserPlaylists.Include(p => p.Playlist).Any(u => u.UserId == currentUserId);    // Check whether current user has any userplaylist if not add the my user playlist to current user's userplaylist
+                if (!currentUserPlayList)
+                {
+                    UserPlaylist newUserPlaylist = new UserPlaylist()
+                    {
+                        UserId = currentUserId,
+                        User = currentUser,
+                        Playlist = myFavoritePlaylist,
+                        PlaylistId = favoritPlaylistId
+                    };
+                    dbContext.UserPlaylists.Add(newUserPlaylist);
+                }
+                return await dbContext.SaveChangesAsync() > 0;
+            }
+            catch(Exception e)
+            {
+                return false;
+            }
+            
         }
     }
 }
